b118419f1cd562baa0bb439fe25f5206965db665
HollowReaper77 <dtomibite@gmail.com>
2026-03-22 14:52:57 +0100

    Lecture 2.6 Done

--- Delivery Dash/Assets/Driver.cs
+++ Delivery Dash/Assets/Driver.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 2f;
     [SerializeField] float moveSpeed = 0.1f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //transform.Rotate(0, 0, 45);
-    }
 
-    // Update is called once per frame
+
     void Update()
     {
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("We are pushing forward");
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("We are pushing backwards");
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("We are pushing left");
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("We are pushing right");
+        }
+
         transform.Rotate(0, 0, steerSpeed);
         transform.Translate(0, moveSpeed, 0);
     }
